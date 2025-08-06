f92968428026f090bd3837ad8c8d779ab8c955a8
Mohsan-Javed <mohsanjavedqureshi@icloud.com>
2025-08-06 22:10:10 +0500

    Added min max program

---
+++ MinMax.py
@@ -0,0 +1,12 @@
+num1 = input(f"Please enter your first number: ")
+num2 = input(f"Please enter your second number: ")
+num3 = input(f"Please enter your third number: ")
+
+mini = min( num1, num2, num3)
+
+print(f" ")
+print(f"The minimum number is: {mini}")
+
+maxi = max( num1, num2, num3)
+
+print(f"The maximum number is: {maxi}")
